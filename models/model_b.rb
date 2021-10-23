16d4a7cd8c67f83fed2742e52085b7701c575551
Chris Kjolhede <kjolhede@Chriss-MacBook-Pro.local>
2021-10-22 21:35:28 -0600

    Add vehicle model hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+"Subaru" = {
+  "model" => 'imprezza',
+  "year" => '2014',
+  "mpg" => 35,
+  "seats" => 4,
+  "body_damage" => true
+  "engine_damage" => false
+  "options" => ['heated seats', 'cruise control', 'undercoat']
+  "trim_level" => ['Premium', 'Sport', 'Limited']
+}
